b4b25bc20bf1c86100cd1cfc02cc063c70608a5f
Shahab6699 <168788928+Shahab6699@users.noreply.github.com>
2024-09-29 23:24:16 +0330

    Update Warp-link1irn.js

--- Warp-link1irn.js
+++ Warp-link1irn.js
@@ -1,6 +1,6 @@
 warp://188.114.96.227:934?ifp=40-80&ifps=40-100&ifpd=4-8&ifpm=m4
 warp://188.114.97.170:894?ifp=40-80&ifps=40-100&ifpd=4-8&ifpm=m4#sh
-warp://162.159.192.128:945?ifp=40-80&ifps=40-100&ifpd=4-8&ifpm=m4#shahab
+warp://188.114.96.154:903?ifp=40-80&ifps=40-100&ifpd=4-8&ifpm=m4#shahab
 warp://188.114.98.234:945/?ifp=1-3&ifpm=g_10_20_30#g_10_20_30
 warp://162.159.192.128:945/?ifp=1-3&ifpm=m5#m5
 warp://104.25.83.162:443/?ifp=1-3&ifpm=h_30#h_30#sh2
